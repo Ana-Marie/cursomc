b21e68c836330c4ed8bcc98b28e1ac7e7bfb7411
Ana-Marie <anamarie1907@gmail.com>
2022-09-21 10:39:56 -0300

    Item pedido e ItemPedido Pk

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -2,7 +2,9 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 @Entity
 public class Pedido implements Serializable {
@@ -26,6 +29,9 @@ public class Pedido implements Serializable {
 	private Endereco enderecoDeEntrega;
 	@ManyToOne
 	private Cliente cliente;
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	
 	public Pedido() {
 		
@@ -74,6 +80,17 @@ public class Pedido implements Serializable {
 	public Cliente getCliente() {
 		return cliente;
 	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+ 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -92,9 +109,6 @@ public class Pedido implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
 	
 	
 	
